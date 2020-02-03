7f30e6e6a049b6bde5823442b9d75ebe659d4dd5
elsiosanchez <elsiosanches@gmail.com>
2020-02-03 14:45:21 -0400

    fixed return value

--- src/convertUtils.js
+++ src/convertUtils.js
@@ -156,7 +156,7 @@ const convertUtils = {
         });
         selectionInstance.addValues(convertedSelection);
       });
-      return selec,tionInstance;
+      return selectionInstance;
     },
 
     /**
